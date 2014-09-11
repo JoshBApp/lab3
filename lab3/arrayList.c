e342bb56f77fbc76ad2b5634319e42b4deafe856
JoshBapp <edwardsjb2@email.appstate.edu>
2014-09-11 13:36:02 -0400

    finished

--- lab3/arrayList.c
+++ lab3/arrayList.c
@@ -1,49 +1,145 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "arrayList.h"
-
+#include <string.h>
 //getSize not accessed outside of this file so declare it to be static
-//static essentially makes it private to this file 
+//static essentially makes it private to this file
 static int getSize(primitiveType type);
 
 arrayList * initialize(primitiveType type)
-{
-   return NULL;
-}
+    {   
+           //dynamically allocate an array
+        arrayList* array = (arrayList*)malloc(sizeof(arrayList));
 
+        //set the array sizes to the values needed
+        array->numElements = 0;
+        array->arraySize= 4;
+        array->type = type;
+         array->elementSize = getSize(type);
+
+        array->array = (void*)malloc(array->elementSize * array->arraySize);
+        return array;
+        
+    }
 int getSize(primitiveType type)
 {
-   return 0;
-}
+    switch(type)
+    {
+        case charType: return sizeof(char);
+        case shortType: return sizeof(short);
+        case intType: return sizeof(int);
+    }
 
-void addElement(arrayList * arylstP, void * element)
+    //what if its not one of them..how??
+    return -1;
+
+}
+void addElement(arrayList * array, void * element)
 {
-   return;
+    if(array->numElements == array->arraySize)
+    {
+        //the array was too large so make a new one 2x the size
+        int newSize = array->arraySize * 2;
+        void* newArray = (void*)malloc(newSize * array->elementSize);
+
+        //copy step
+        memcpy(newArray, array->array, array->arraySize * array->elementSize);
+        free(array->array);
+        array->array = newArray;
+        array->arraySize =newSize;
+    }
+
+    //depending on the type put in the positon
+    switch(array->type)
+    {
+        case charType:
+        {
+            char* cher = (char*)(array->array);
+            char* elePtr = (char*)element;
+            cher[array->numElements]= *elePtr;
+            break;
+
+        }
+        
+        case shortType:
+        {
+            short* sho = (short*)(array->array);
+            short* shElePtr = (short*)element;
+            sho[array->numElements] = *shElePtr;
+            break;
+        }
+
+        case intType:
+        {
+            int* in = (int*)(array->array);
+            int* inElePtr = (int*)element;
+            in[array->numElements] = *inElePtr;
+            break;
+        }
+       }
+        array->numElements++;
 }
 
-void removeElement(arrayList * arylstP, int index)
+
+void removeElement(arrayList * array, int index)
 {
-   return;
-}
-      
+    int i;
+    for(i= index; i < array->numElements - 1; i++)
+    {
+        switch(array->type)
+        {
+            case charType:
+            {
+                char* c = (char*) array->array;
+                c[i] = c[i + 1];
+                break;
+               
+            }
 
+            case shortType:
+            {
+                short* s = (short*) array->array;
+                s[i] = s[i+1];
+                break;
+            }
+
+            case intType:
+            {
+                int* h = (int*) array->array;
+                h[i] = h[i+1];
+                break;
+
+            }
+        }
+     }
+        array->numElements--;
+
+    
+
+}
 void printArray(arrayList * arylstP)
 {
-   int i;
-   printf("array size: %d, num elements: %d contents: ", 
-          arylstP->arraySize, arylstP->numElements);
-   for (i = 0; i < arylstP->numElements; i++)
-   {
-      if (arylstP->type == charType)
-         //fill in the missing code that gets the element and &s it with 0xff
-         printf("%02x ", 0xff);
-      else if (arylstP->type == shortType)
-         //fill in the missing code that gets the element and &s it with 0xffff
-         printf("%04x ", 0xffff);
-      else if (arylstP->type == intType)
-         //fill in the missing code that gets the element and &s it with 0xffffffff
-         printf("%08x ", 0xffffffff);
-   }
-   printf("\n");
-}
 
+int i;
+printf("array size: %d, num elements: %d contents: ",
+arylstP->arraySize, arylstP->numElements);
+for (i = 0; i < arylstP->numElements; i++)
+    {
+    if (arylstP->type == charType)
+    {
+        printf("%02x ",((char *) (arylstP ->array))[i] & 0xff);
+    }
+
+    else if (arylstP->type == shortType)
+    {
+    //fill in the missing code that gets the element and &s it with 0xffff
+    
+    printf("%04x ", ((short *) (arylstP-> array))[i] & 0xffff);
+    }
+    else if (arylstP->type == intType)
+    {
+    printf("%08x ", ((int *)(arylstP->array))[i] & 0xffffffff);
+    }
+    }
+    printf("\n");
+}
